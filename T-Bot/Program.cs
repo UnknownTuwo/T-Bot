13f10c5ec16ce0cb76c41fd776fdb32175453040
Tuwo <markus.koidl@gmx.at>
2021-12-05 13:14:40 +0100

    added DSharpPlus

--- T-Bot/Program.cs
+++ T-Bot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
 
 namespace T_Bot
 {
@@ -6,7 +8,22 @@ namespace T_Bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration())
+            {
+                Token = Environment.GetEnvironmentVariable("DiscordToken"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            };
+            
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
         }
     }
 }
